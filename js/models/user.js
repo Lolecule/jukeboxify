a58d43a35871670956d75463b26621d74364ad82
Steve Parrington <steveparrington@gmail.com>
2014-10-13 23:41:53 +0100

    Add user model with authentication logic

---
+++ js/models/user.js
@@ -0,0 +1,21 @@
+User = Backbone.Model.extend({
+  urlRoot: 'https://api.spotify.com/v1',
+  defaults: {
+    id: 'me'
+  }
+  initialize: function() {
+    var params = $.deparam.querystring();
+    if (params.access_token != null) {
+      this.set({ token: params.access_token })
+    }
+  }
+});
+
+var user = new User();
+var authHeader = {'Authorization': 'Bearer ' + user.get("token")};
+user.fetch({
+  headers: authHeader,
+  success: function(user) {
+    alert("Hello! " + user.get("display_name"))
+  }
+})
